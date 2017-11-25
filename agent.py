c2a42dc7cfaa2c91fb79ee695385ab5fecdbdbf3
Nikita Petrenko <npetrenko97@gmail.com>
2017-11-25 21:54:21 +0300

    init commit

---
+++ agent.py
@@ -0,0 +1,40 @@
+import numpy as np
+import random
+
+class Env:
+    def __init__ (self, *agents):
+        self.agents = agents
+    def step(self, see_all=True):
+        if not see_all:
+            return [a.step().emmit() for a in self.agents if random.random() < 0.7]
+        else:
+            return [a.step().emmit() for a in self.agents]
+    
+class Agent:
+    def __init__(self, im_x, im_y):
+        self.im_x = im_x
+        self.im_y = im_y
+        
+        w, h = random.randrange(2, 5)*2, random.randrange(2, 5)*2
+        self.w = w
+        self.h = h
+        self.dim = np.array([w,h], dtype='float64')
+        
+        pos = np.array([random.randrange(im_x - w), random.randrange(im_y - h)], dtype='float64')
+        self.pos = pos
+        self.vel = np.random.normal(size=2)
+        
+    def emmit(self):
+        xyxy = list(self.pos) + list(self.pos + self.dim)
+        xyxy = np.array(xyxy)
+        xyxy += np.random.normal(size=xyxy.shape)*np.array([self.w, self.h]*2)/8
+        xyxy[3] = max(xyxy[1]+1, xyxy[3])
+        xyxy[2] = max(xyxy[0]+1, xyxy[2])
+        return xyxy
+    
+    def step(self):
+        self.vel += np.random.normal(size=2)/4
+        self.vel = np.clip(self.vel, -1, 1)
+        self.pos += self.vel
+        self.pos = np.clip(self.pos, 0, np.array([self.im_x - self.w, self.im_y - self.h]))
+        return self
---
+++ pfilter.py
@@ -0,0 +1,59 @@
+import numpy as np
+
+class ParticleFilter:
+    def __init__ (self, nparticles, hidden_dim, evol_model, observe_model, resample_2entropy = 0.1):
+        self.nparticles = nparticles
+        self.hidden_dim = hidden_dim
+        self.evol_model = evol_model
+        self.observe_model = observe_model
+        self.resample_2entropy = resample_2entropy
+        self.resamples = 0
+        self.step_calls = 0
+        
+        self.particles = np.random.normal(size=[nparticles, hidden_dim])# + np.array([2,0], 'float64')
+        self.logweights = np.zeros([nparticles], dtype='float64')
+        
+    def step(self, observe):
+        self.step_calls += 1
+        
+        new_logweights = self.observe_model(self.particles, observe)
+        self.logweights += new_logweights
+        self.logweights -= self.logweights.mean()
+        self.logweights = np.clip(self.logweights, -20, 20)
+        
+        #resample if entropy is too low:
+        weights = np.exp(self.logweights)
+        weights /= weights.sum()
+
+        ent = np.sum(-weights*np.log2(weights))
+        tent = 2**ent
+
+        if tent/self.nparticles < self.resample_2entropy:
+            self.resample()
+        
+        self.particles = self.evol_model(self.particles)
+        
+        weights = np.exp(self.logweights)
+        weights /= weights.sum()
+        return self.particles, weights
+    
+    def filtrate(self, observations):
+        ps = []
+        
+        for obs in observations:
+            p, w = self.step(obs)
+            ix = np.random.choice(np.arange(len(p)), size=len(p), p=w)
+            p = p[ix]
+            ps.append(p)
+        ps = np.array(ps)
+        print(self.resamples/self.step_calls)
+        return ps
+    
+    def resample(self):
+        self.resamples += 1
+        weights = np.exp(self.logweights)
+        #print(weights.sum())
+        weights /= weights.sum()
+        ix = np.random.choice(np.arange(self.nparticles), size=self.nparticles, p=weights)
+        self.particles = self.particles[ix]
+        self.logweights = np.zeros_like(self.logweights)
